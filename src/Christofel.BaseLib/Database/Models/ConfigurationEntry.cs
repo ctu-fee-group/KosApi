68d824ac10b81a495450d5e6c96395e7d113a004
František Boháček <fandabohacek@gmail.com>
2021-08-18 21:12:54 +0200

    chore: remove unnecessary file

--- src/Christofel.BaseLib/Database/Models/ConfigurationEntry.cs
+++
@@ -1,9 +0,0 @@
-namespace Christofel.BaseLib.Database.Models
-{
-    public class ConfigurationEntry
-    {
-        public string Name { get; set; } = null!;
-
-        public string Value { get; set; } = null!;
-    }
-}
