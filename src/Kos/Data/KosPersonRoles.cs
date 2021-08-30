94b500e6a1d6f81e9cdde9873f5c0951f57bf567
František Boháček <fandabohacek@gmail.com>
2021-08-30 14:10:12 +0200

    fix: use list instead of IReadOnlyList as Xml serializer cannot parse it

--- src/Kos/Data/KosPersonRoles.cs
+++ src/Kos/Data/KosPersonRoles.cs
@@ -5,7 +5,7 @@ using Kos.Atom;
 namespace Kos.Data
 {
     public record KosPersonRoles(
-        [property: XmlElement("student")] IReadOnlyList<AtomLoadableEntity<KosStudent>> Students,
+        [property: XmlElement("student")] List<AtomLoadableEntity<KosStudent>> Students,
         [property: XmlElement("teacher")] AtomLoadableEntity<KosTeacher>? Teacher)
     {
         public KosPersonRoles()
