23cc9d9a87761cc26f0d55d359db5eb2364da17d
František Boháček <fandabohacek@gmail.com>
2021-09-24 17:25:30 +0200

    fix: make atom loadable entity deserializable by System.Xml.Serialization

--- src/Kos/Atom/AtomLoadableEntity.cs
+++ src/Kos/Atom/AtomLoadableEntity.cs
@@ -4,27 +4,34 @@
 //  Copyright (c) Christofel authors. All rights reserved.
 //  Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Xml.Serialization;
 
 namespace Kos.Atom
 {
     /// <summary>
+    /// The base loadable entity class used just for xml serializer to initialize it correctly.
+    /// </summary>
+    /// <param name="Href"></param>
+    /// <param name="Title"></param>
+    [Obsolete]
+    public record AtomLoadableEntity(string? Href, string? Title);
+
+    /// <summary>
     /// Represents reference to an entity that can be loaded using the Atom Api.
     /// </summary>
     /// <typeparam name="TContent">The type of the content that will be loaded.</typeparam>
-    public record AtomLoadableEntity<TContent>
-    (
-        [property: XmlAttribute("href", Namespace = "http://www.w3.org/1999/xlink")]
-        string? Href,
-        [property: XmlText] string? Title
-    )
+#pragma warning disable 612
+    public record AtomLoadableEntity<TContent> : AtomLoadableEntity
+#pragma warning restore 612
         where TContent : new()
     {
+
         /// <summary>
         /// Initializes a new instance of the <see cref="AtomLoadableEntity{T}"/> class.
         /// </summary>
         public AtomLoadableEntity()
-            : this(default!, default)
+            : base(default!, default!)
         {
         }
     }
