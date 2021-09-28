b6de75713e7527aa8cba80b9b1184414e07fdb47
František Boháček <fandabohacek@gmail.com>
2021-09-28 18:10:31 +0200

    feat: add course enrollment and exam registration

---
+++ src/Kos/Data/CourseEnrollment.cs
@@ -0,0 +1,37 @@
+//
+//   CourseEnrollment.cs
+//
+//   Copyright (c) Christofel authors. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Xml.Serialization;
+using Kos.Atom;
+
+namespace Kos.Data
+{
+    /// <summary>
+    /// Represents kos course enrollment entity.
+    /// </summary>
+    /// <param name="Assessment">Whether the student has completed the course successfully.</param>
+    /// <param name="Completed">Whether the student has completed the course. (successfully or unsuccessfully)</param>
+    /// <param name="Extern">Unknown.</param>
+    /// <param name="Semester">The semester the course was enrolled in.</param>
+    /// <param name="Course">The enrolled course.</param>
+    public record CourseEnrollment
+    (
+        [property: XmlElement("assessment")] bool Assessment,
+        [property: XmlElement("completed")] bool Completed,
+        [property: XmlElement("extern")] bool Extern,
+        [property: XmlElement("semester")] AtomLoadableEntity<Semester> Semester,
+        [property: XmlElement("course")] AtomLoadableEntity<Course> Course
+    ) : KosContent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CourseEnrollment"/> class.
+        /// </summary>
+        public CourseEnrollment()
+            : this(default!, default!, default!, default!, default!)
+        {
+        }
+    }
+}
---
+++ src/Kos/Data/ExamRegistration.cs
@@ -0,0 +1,29 @@
+//
+//   ExamRegistration.cs
+//
+//   Copyright (c) Christofel authors. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Xml.Serialization;
+using Kos.Atom;
+
+namespace Kos.Data
+{
+    /// <summary>
+    /// Represents kos exam registration entity.
+    /// </summary>
+    /// <param name="Exam">The registered exam term.</param>
+    public record ExamRegistration
+    (
+        [property: XmlElement("exam")] AtomLoadableEntity<Exam> Exam
+    ) : KosContent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExamRegistration"/> class.
+        /// </summary>
+        public ExamRegistration()
+            : this((AtomLoadableEntity<Exam>)default!)
+        {
+        }
+    }
+}
