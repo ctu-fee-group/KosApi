1405ea00060215614830e4cd00233140a47fc403
František Boháček <fandabohacek@gmail.com>
2021-09-28 13:22:06 +0200

    feat: add atom author

---
+++ src/Kos/Atom/AtomAuthor.cs
@@ -0,0 +1,19 @@
+//
+//   AtomAuthor.cs
+//
+//   Copyright (c) Christofel authors. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Kos.Atom
+{
+    /// <summary>
+    /// Represents atom entity author.
+    /// </summary>
+    public class AtomAuthor
+    {
+        /// <summary>
+        /// Gets or sets the name of the author.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
